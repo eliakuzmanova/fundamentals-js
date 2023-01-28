cf2a826e63222ad2568f6ea86151deb6f353678f
Elia Kuzmanova <eliamitova7@gmail.com>
2023-01-28 23:08:11 +0100

    add homework

---
+++ NxN Matrix.js
@@ -0,0 +1,13 @@
+function nxnMatrix(n) {
+    for(let i = 1; i <= n; i++) {
+        let print = "";
+        for(let y = 1; y <= n; y++) {
+            print += n + " ";
+        }
+        console.log(print);
+    }
+
+}
+nxnMatrix(3)
+nxnMatrix(7)
+nxnMatrix(2)
